42bb27aa4f29b3948ed156cf7640eeb911b721c2
lemonsqueeze <lemonsqueeze@gmx.com>
2010-09-25 20:09:00 +0200

    noscript_menu_mouseover.js - figured out how to show/hide menu using mouseover/mouseout instead of clicking - create menu only when needed - cleanup, refactoring

--- jsarmor.js
+++ jsarmor.js
@@ -25,14 +25,28 @@
         }
         return '';
     }
+    
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
 
+    function reload_page()
+    { history.go(0); }
+    
     // block_all, filtered, allow_all    
     var mode = readCookie('noscript_mode', 'g');
     if (mode == '') { mode = 'block_all'; }
 
-    function eraseCookie(name) {
-        createCookie(name, "", -1);
-    }
+    function set_mode(new_mode, reload)
+    {
+      mode = new_mode;
+      if (new_mode == 'filtered' && !readCookie('noscript', 'g'))
+	allow_domain(current_domain);
+      createCookie('noscript_mode', mode, 365, null, 'g');
+      set_icon_style(button_image, mode);
+      show_hide_menu(false);
+      reload_page();
+    }    
 
     function f(x, y, z) {
         var r = (x.innerText.indexOf('#X# -') == 0);
@@ -49,11 +63,23 @@
         }
     }
 
+    function allow_domain(domain)
+    {
+      var h = readCookie('noscript', 'g') ? readCookie('noscript', 'g') : '';
+      createCookie('noscript', domain, 365, h, 'g');
+    }
+
+    function block_domain(domain)
+    {
+      var h = readCookie('noscript', 'g') ? readCookie('noscript', 'g') : '';
+      h = h.replace(domain, '');
+      createCookie('noscript', h, (h == '' ? -1: 365), null, 'g');      
+    }
+    
     function cus() {
         var b = document.getElementById('noscriptselect');
         var zi = (event.ctrlKey || this.value == 'Server') ? b[b.selectedIndex].getAttribute('title') : (event.shiftKey || this.value == 'All') ? '': b.value;
 
-        var h = readCookie('noscript', 'g') ? readCookie('noscript', 'g') : '';
         if (document.getElementById('nelbb').value == 'Block')
         {
 	  // block server
@@ -62,8 +88,7 @@
 	      f(x, 'X');
 	    }
 	  }
-	  h = h.replace(zi, '');
-	  createCookie('noscript', h, (h == '' ? -1: 365), null, 'g');
+	  block_domain(zi);
         }
         else
 	{
@@ -72,7 +97,7 @@
 	      f(x);
 	    }
 	  }
-	  createCookie('noscript', zi, 365, h, 'g');
+	  allow_domain(zi);
 	}
         tog();
     }
@@ -101,19 +126,29 @@
     }
 
     var current_domain = get_domain(location.hostname);
+
+    function filtered_mode_should_allow(scr_domain) {
+      var v = readCookie('noscript', 'g');
+      return (v && v.indexOf(scr_domain) != -1);
+    }
     
     function should_allow(scr_domain) {
-      var v = readCookie('noscript', 'g');
       if (mode == 'block_all') { return false; }
-      if (mode == 'filtered') {
-	return ((scr_domain == current_domain) ||
-		(v && v.indexOf(scr_domain) != -1));
-      }
+      if (mode == 'filtered') { return filtered_mode_should_allow(scr_domain); }
       if (mode == 'allow_all') { return true; }
       alert('should not be reached!');
     }
 
-    function set_icon_style(img) {
+    function new_icon(mode)
+    {
+      var icon = document.createElement('img');
+      set_icon_style(icon, mode);
+      return icon;
+    }
+    
+    var button_image = new_icon(mode);
+    
+    function set_icon_style(img, mode) {
       var icon;      
       if (mode == 'block_all') {
 	// icon = "Blocked";
@@ -134,9 +169,11 @@
       // r.style = "background:-o-skin('" + icon + "');display:inline-block;";
     }
 
-    function add_menu_item(nsmenu, text, f) {
+    function add_menu_item(nsmenu, text, indent, f, icon) {
+      // FIXME: add icon
       var item = document.createElement('p');
-      item.style = "margin-top:0px; margin-bottom:0px;";
+      item.style = "margin-top:0px; margin-bottom:0px;" +
+        (indent ? "padding-left:" + (indent * 10) + "px;": "");
 //      item.style.backgroundColor = 'transparent';
       // FIXME; make text non selectable
       item.text = text;
@@ -148,8 +185,93 @@
       }
       // make text non selectable
       item.onmousedown = function(){ return false; };
+      if (icon)
+        { nsmenu.appendChild(icon); }
       nsmenu.appendChild(item);
       return item;
+    }
+
+    var nsmenu;
+    function create_menu()
+    {
+	nsmenu = document.createElement('div');		
+	nsmenu.align = 'left';
+	nsmenu.style="border-width: 2px; border-style: outset; border-color: gray; background:#abb9ca;";
+        nsmenu.style.display = 'none';
+
+	nsmenu.onmouseout = function(e) {
+
+	  if (!e) var e = window.event;
+	  // object we're moving out of
+	  // var tg = (window.event) ? e.srcElement : e.target;
+	  // if (tg != nsmenu) // moving out of one its children.
+	  //  return; we actually need that case!
+	  
+	  // e.relatedTarget: object we're moving to.
+	  var reltg = e.relatedTarget;
+	  while (reltg != nsmenu && reltg.nodeName != 'HTML')
+	    reltg= reltg.parentNode
+	  if (reltg == nsmenu) return; // moving out of the div into a child layer
+	  
+	  show_hide_menu(false);
+	};
+	
+//	nsmenu.border = '1';
+//	nsmenu.style = 'border-spacing:0; border-top-color:#dddddd; border-left-color:#dddddd; border-bottom-color:#888888; border-right-color:#888888;';
+
+//	nsmenu.style = 'border-top-color:#dddddd; border-left-color:#dddddd; border-bottom-color:#888888; border-right-color:#888888; border-top-width:2px;';
+
+	add_menu_item(nsmenu, "Choose Mode:");
+	add_menu_item(nsmenu, "block all", 2, function(){ set_mode('block_all'); }, new_icon('block_all'));
+	add_menu_item(nsmenu, "filtered", 2, function(){ set_mode('filtered'); }, new_icon('filtered'));
+
+	for (var i = 0; i < domains.length; i++)
+	{
+	  var ba = 'allow ';
+	  var d = domains[i];
+	  var f = function() { allow_domain(this.domain); set_mode('filtered'); };
+	  if (filtered_mode_should_allow(d))
+	    {
+	      ba = 'block ';
+	      f = function() { block_domain(this.domain); set_mode('filtered'); };
+	    }
+	  var item = add_menu_item(nsmenu, ba + d, 4, f);
+	  item.domain = d;
+	}
+	
+	add_menu_item(nsmenu, "allow all", 2, function(){ set_mode('allow_all'); }, new_icon('allow_all'));
+
+	var td = document.getElementById('td_nsmenu');
+	td.appendChild(nsmenu);	
+    }
+
+    function show_hide_menu(show, toggle)
+    {
+      if (!nsmenu)
+	create_menu();
+      var d = (show ? 'inline-block' : 'none');
+      if (toggle) 
+        { d = (nsmenu.style.display == 'none' ? 'inline-block' : 'none'); }
+      nsmenu.style.display = d;
+    }
+    
+    // array of arrays: scripts[0] has all the scripts for domain domains[0] etc
+    var scripts = [];
+    var domains = [];
+    function add_script(url, domain)
+    {
+      for (var i = 0; i < domains.length; i++)
+      {
+	if (domains[i] == domain)
+	{
+	  scripts[i].push(url);
+	  return;
+	}
+      }
+
+      domains.push(domain);
+      scripts.push([]);
+      scripts[i].push(url);      
     }
 
     var scI = 0;
@@ -205,7 +327,8 @@
 	  total_external++;
 	  if (!allowed) { blocked_external++; }
 	}
-	
+
+	add_script(x, scE);
         if (allowed)
         { scA.push(scIil); }
         else {
@@ -270,7 +393,7 @@
         scIui.appendChild(scIbut4);
         var r = document.createElement('button');
 
-        var tooltip = "[" + current_domain + "] " + blocked_current_domain;
+        var tooltip = "Scripts on current page: [" + current_domain + "] " + blocked_current_domain;
 	if (blocked_current_domain != total_current_domain)
 	  {  tooltip += "/" + total_current_domain; }
 	tooltip += " blocked";
@@ -285,22 +408,21 @@
 	  { tooltip += " (" + loaded_external + " loaded)"; }
 	r.title = tooltip;
 	
-	var img = document.createElement('img');
-	r.appendChild(img);
-	set_icon_style(img);
-
-	function set_mode(new_mode, reload)
-	{
-	  mode = new_mode;
-	  createCookie('noscript_mode', mode, 365, null, 'g');
-	  set_icon_style(img);
-	  nsmenu.style.display = 'none';
-	  // FIXME, reload
-	}
+	r.appendChild(button_image);
     
         r.onclick = function() {
-	  if (event.ctrlKey)
-	  { // show/hide ui
+	  if (event.shiftKey)
+	  { // cycle through the modes
+	    // FIXME: should wait until shift is released to reload page
+	    if (mode == 'block_all') { set_mode('filtered'); }
+	    else if (mode == 'filtered') { set_mode('allow_all'); }
+	    else if (mode == 'allow_all') { set_mode('block_all'); }
+	    return;
+	  }
+
+//	  if (event.ctrlKey)
+	  if (1)
+	  { // show/hide extended ui
 	    var x = document.getElementById('noiframeui');
 	    if (x.style.display == 'none') {
 	      x.style.display = 'inline-block';
@@ -311,48 +433,16 @@
 	    x.style.display = 'none';
             }
 	    return;
-	  }	
-
-	  if (event.shiftKey)
-	  { // cycle through the modes
-	    if (mode == 'block_all') { set_mode('filtered'); }
-	    else if (mode == 'filtered') { set_mode('allow_all'); }
-	    else if (mode == 'allow_all') { set_mode('block_all'); }
-	    return;
-	  }
-	  
-	  // normal click, show/hide menu
-	  if (nsmenu.style.display == 'inline-block')
-	    { nsmenu.style.display = 'none'; }
-	  else
-	    { nsmenu.style.display = 'inline-block'; }	
+	  }		  
 	}
 
-//	document.write("<div id=mydiv;>Foo Bar Baz</div>");
-//	alert("trying to read div id")
+	r.onmouseover = function() { show_hide_menu(true); };
 	
-	var nsmenu = document.createElement('div');
-	nsmenu.align = 'left';
-	nsmenu.style="border-width: 2px; border-style: outset; border-color: gray; background:#abb9ca;";
-        nsmenu.style.display = 'none';
-	
-//	nsmenu.border = '1';
-//	nsmenu.style = 'border-spacing:0; border-top-color:#dddddd; border-left-color:#dddddd; border-bottom-color:#888888; border-right-color:#888888;';
-
-//	nsmenu.style = 'border-top-color:#dddddd; border-left-color:#dddddd; border-bottom-color:#888888; border-right-color:#888888; border-top-width:2px;';
-
-	add_menu_item(nsmenu, "Choose Mode:");
-	var f = function(){ set_mode('block_all'); };	
-	add_menu_item(nsmenu, "block all", f);
-	var f = function(){ set_mode('filtered'); };		
-	add_menu_item(nsmenu, "filtered", f);
-	var f = function(){ set_mode('allow_all'); };		
-	add_menu_item(nsmenu, "allow all", f);
-
 	var tr = document.createElement('tr');
 	var td = document.createElement('td');
+	td.id = 'td_nsmenu';
 	td.align = 'right';
-	td.appendChild(nsmenu);
+
 	tr.appendChild(td);
         scIele.appendChild(tr);
 
